22efbe281aaf5d5bd3fb2fc50085e135655c39e0
Guysnacho <guysnacho2@gmail.com>
2022-10-20 00:08:33 -0500

    Add tailwind, pretter, configs, and layout

---
+++ components/individual/HomeCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Delectus,
+    consequatur beatae in provident alias impedit aut magni debitis excepturi
+    quibusdam itaque adipisci quasi natus? Et exercitationem earum nobis
+    perferendis explicabo!
+  </div>
+</template>
