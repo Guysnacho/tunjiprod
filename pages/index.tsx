89b7ac23b84cdba8681acbcbf23d0c014acf81e3
Samuel Adetunji <guysnacho2@gmail.com>
2022-12-02 03:15:23 -0600

    Add hero text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-import { Alert, AlertTitle, Grid } from "@mui/material";
+import {
+  Alert,
+  AlertTitle,
+  Container,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import type { NextPage } from "next";
 
 import Head from "next/head";
 import { Octokit } from "octokit";
-import { useEffect } from "react";
+//import { useEffect } from "react";
 import Single from "../components/individual/Single";
 
 const octokit = new Octokit();
@@ -14,7 +21,7 @@ const Home: NextPage = () => {
     ["repoData"],
     () =>
       octokit.request(
-        "GET /users/Guysnacho/repos?sort=created_at&direction=desc&per_page=10"
+        "GET /users/Guysnacho/repos?sort=created_at&direction=desc&per_page=5"
       ),
     {}
   );
@@ -34,15 +41,44 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {error ? (
+      <Container
+        component={Paper}
+        sx={{
+          width: { xs: "80vw", sm: "70vw", md: "65vw", lg: "61vw" },
+          py: 3,
+          my: 2,
+        }}
+      >
+        <Typography
+          variant="h3"
+          textAlign="center"
+          sx={{ userSelect: "none", mb: 2 }}
+        >
+          Inspo
+        </Typography>
+        <Typography
+          variant="body1"
+          textAlign="center"
+          sx={{ userSelect: "none" }}
+        >
+          I kinda wanted to make a portfolio site. It'd be kinda boring if I
+          only put coding projects though so I'm prolly gonna add my personal
+          projects too. I put a lil smidgin of my soul in all my work but now
+          that I've kind of hit a developer groove, I might as well put it in
+          one place.
+        </Typography>
+      </Container>
+      <Container sx={{ py: 3 }}>
+        <Typography variant="h3" textAlign="center" sx={{ userSelect: "none" }}>
+          Projects
+        </Typography>
+      </Container>
+      {error ? ( // Failed to fetch repos
         <Alert color="info" sx={{ mx: "auto" }} variant="filled">
           <AlertTitle>Uh oh, Spaghetti-O's™</AlertTitle>
           {`${error}\nMan that's unlucky, try a refresh or sumn :)`}
         </Alert>
-      ) : (
-        <></>
-      )}
-
+      ) : undefined}
       <Grid
         container
         sx={{
@@ -55,7 +91,6 @@ const Home: NextPage = () => {
             item
             xs={12}
             sm={6}
-            md={4}
             px="auto"
             py={3}
             m="auto"
