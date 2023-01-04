865dac647097aff3c9249ee2ea30fd09dd5a4988
Samuel Adetunji <guysnacho2@gmail.com>
2023-01-04 00:27:58 -0600

    Add lastfm search and bonus fields

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,17 +1,18 @@
-import { useRouter } from "next/dist/client/router";
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabaseClient";
 import {
+  Box,
   Button,
   Card,
   CardContent,
-  FormControl,
   Grid,
-  Stack,
+  List,
+  Paper,
   TextField,
   Typography,
 } from "@mui/material";
-import { authCodes } from "../lib/constants";
+import { useRouter } from "next/dist/client/router";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import { Song } from "../components/individual/Single";
 
 /**
  * @fileoverview Where all the customization happens
@@ -21,7 +22,16 @@ import { authCodes } from "../lib/constants";
 const Admin = () => {
   const router = useRouter();
 
+  const [songs, setSongs] = useState([]);
   const [songTitle, setSongTitle] = useState("");
+  const [artist, setArtist] = useState("");
+  const [two_cents, setTwo_cents] = useState("");
+  const [album, setAlbum] = useState("");
+  const [album_art, setAlbum_art] = useState("");
+  const [written_by, setWritten_by] = useState("");
+  const [produced_by, setProduced_by] = useState("");
+
+  let fasdf: Song;
 
   // Redirect if not authed
   useEffect(() => {
@@ -30,6 +40,18 @@ const Admin = () => {
     });
   }, []);
 
+  const handleSubmit = () =>
+    fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${songTitle}&api_key=${process.env.NEXT_PUBLIC_LAST_FM_TOKEN}&format=json`
+    )
+      .then((res) => res.json())
+      .then((data) => setSongs(data.results.trackmatches))
+      .catch((err) => console.error(err));
+
+  useEffect(() => {
+    console.log(songs);
+  }, [songs]);
+
   return (
     <Grid container>
       <Grid item xs={12} sx={{ my: 3 }}>
@@ -37,34 +59,116 @@ const Admin = () => {
           Welcome Back 😌
         </Typography>
       </Grid>
-      <Grid item xs={12} md={4} sx={{ my: 3, px: "auto" }}>
-        <Card sx={{ mx: "auto" }}>
-          <CardContent>
-            <Stack>
-              <FormControl sx={{ py: 5, mx: "auto", width: "75%" }}>
-                <TextField
-                  id="filled-song-input"
-                  variant="filled"
-                  label="Song"
-                  type="text"
-                  value={songTitle}
-                  onChange={(e) => {
-                    setSongTitle(e.target.value);
-                  }}
-                  required
-                />
-                <Button
-                  variant="text"
-                  aria-label="login"
-                  type="submit"
-                  onSubmit={() => console.log("Song Submitted")}
-                >
-                  Submit SOTD
-                </Button>
-              </FormControl>
-            </Stack>
+      <Grid item xs={12} sx={{ my: 3, px: "auto" }}>
+        <Card
+          sx={{
+            width: { xs: "65vw", sm: "45vw", md: "30vw", lg: "25vw" },
+            mx: "auto",
+          }}
+        >
+          <CardContent
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Typography
+              variant="h5"
+              textAlign="center"
+              sx={{ userSelect: "none" }}
+            >
+              Search
+            </Typography>
+            <TextField
+              id="filled-song-input"
+              variant="filled"
+              label="Song"
+              type="text"
+              value={songTitle}
+              onChange={(e) => {
+                setSongTitle(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-artist-input"
+              variant="filled"
+              label="Artist"
+              type="text"
+              value={artist}
+              onChange={(e) => {
+                setArtist(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-description-input"
+              variant="filled"
+              multiline
+              minRows={3}
+              maxRows={5}
+              label="Description"
+              type="text"
+              value={two_cents}
+              onChange={(e) => {
+                setTwo_cents(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-album-input"
+              variant="filled"
+              label="Album"
+              type="text"
+              value={album}
+              onChange={(e) => {
+                setAlbum(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-album-art-input"
+              variant="filled"
+              label="Album Art"
+              type="text"
+              value={album_art}
+              onChange={(e) => {
+                setAlbum_art(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-song-input"
+              variant="filled"
+              label="Song"
+              type="text"
+              value={written_by}
+              onChange={(e) => {
+                setWritten_by(e.target.value);
+              }}
+              required
+            />
+            <TextField
+              id="filled-song-input"
+              variant="filled"
+              label="Song"
+              type="text"
+              value={produced_by}
+              onChange={(e) => {
+                setProduced_by(e.target.value);
+              }}
+              required
+            />
+            <Button variant="text" aria-label="login" type="submit">
+              Search
+            </Button>
           </CardContent>
         </Card>
+      </Grid>
+      <Grid xs={12} item>
+        <Box component={Paper}>
+          <List sx={{ width: "75%", maxWidth: 400 }}></List>
+        </Box>
       </Grid>
     </Grid>
   );
