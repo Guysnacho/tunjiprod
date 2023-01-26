45f64f9bdbad9c2a7694e8329e17dd5675988c81
Samuel Adetunji <guysnacho2@gmail.com>
2023-01-25 21:50:44 -0600

    Add password reset

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -12,6 +12,7 @@ import {
   Typography,
 } from "@mui/material";
 import { authCodes } from "../lib/constants";
+import Head from "next/head";
 
 /**
  * @fileoverview Where all the customization happens
@@ -31,42 +32,49 @@ const Admin = () => {
   }, []);
 
   return (
-    <Grid container>
-      <Grid item xs={12} sx={{ my: 3 }}>
-        <Typography variant="h4" textAlign="center">
-          Welcome Back 😌
-        </Typography>
+    <>
+      <Head>
+        <title>Studio | Tunji Productions</title>
+        <meta name="description" content="Where the magic happens" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Grid container>
+        <Grid item xs={12} sx={{ my: 3 }}>
+          <Typography variant="h4" textAlign="center">
+            Welcome Back 😌
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={4} sx={{ my: 3, px: "auto" }}>
+          <Card sx={{ mx: "auto" }}>
+            <CardContent>
+              <Stack>
+                <FormControl sx={{ py: 5, mx: "auto", width: "75%" }}>
+                  <TextField
+                    id="filled-song-input"
+                    variant="filled"
+                    label="Song"
+                    type="text"
+                    value={songTitle}
+                    onChange={(e) => {
+                      setSongTitle(e.target.value);
+                    }}
+                    required
+                  />
+                  <Button
+                    variant="text"
+                    aria-label="login"
+                    type="submit"
+                    onSubmit={() => console.log("Song Submitted")}
+                  >
+                    Submit SOTD
+                  </Button>
+                </FormControl>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={4} sx={{ my: 3, px: "auto" }}>
-        <Card sx={{ mx: "auto" }}>
-          <CardContent>
-            <Stack>
-              <FormControl sx={{ py: 5, mx: "auto", width: "75%" }}>
-                <TextField
-                  id="filled-song-input"
-                  variant="filled"
-                  label="Song"
-                  type="text"
-                  value={songTitle}
-                  onChange={(e) => {
-                    setSongTitle(e.target.value);
-                  }}
-                  required
-                />
-                <Button
-                  variant="text"
-                  aria-label="login"
-                  type="submit"
-                  onSubmit={() => console.log("Song Submitted")}
-                >
-                  Submit SOTD
-                </Button>
-              </FormControl>
-            </Stack>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+    </>
   );
 };
 export default Admin;
