bac7932d5f033f6c7b3b316ae1271da501693088
Guysnacho <tunjiprod@gmail.com>
2025-02-06 05:51:54 -0600

    Get starter footer out the way

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,21 +14,22 @@ const footerLinks = [
     },
     {
         route: '/login',
-        icon: 'pi pi-dooropen'
+        icon: 'pi pi-sign-in'
     },
 ]
 </script>
 
 <template>
-    <footer class="mt-32 bg-[#452e1f] text-white text-center h-36">
+    <footer class="mt-32 py-5 bg-[#452e1f] text-white text-center h-48 border-t-yellow-500 border-t-2">
         <div class="m-auto space-y-5">
-            <p class="my-auto">Let's Chat!</p>
-            <p class="my-auto">Made with ❤ by Tunji Productions LLC</p>
-            <div class="flex space-x-10" v-for="item in footerLinks">
-                <a v-ripple :href="item.route" target="_blank">
+            <p>Let's Chat!</p>
+            <div class="flex justify-around space-x-10 mx-auto">
+                <a v-for="item in footerLinks" v-ripple :href="item.route" target="_blank" class="px-2">
                     <span :class="item.icon" />
                 </a>
             </div>
+            <p>Made with ❤ by Tunji Productions LLC.</p>
+            <p class="text-sm">&copy; by Tunji Productions LLC. All Rights Reserved.</p>
         </div>
     </footer>
 </template>
