deb8aea3dfce4fa9c4534f324c964e012671e5ce
Guysnacho <tunjiprod@gmail.com>
2025-02-08 03:50:51 -0600

    Add manifesto

--- components/Project.vue
+++ components/Project.vue
@@ -1,7 +1,13 @@
 <template>
-  <Card variant="outlined"
+  <Card
     class="border-[#ff8708] hover:border-[#f7d882] shadow hover:shadow-md bg-linear-205 from-[rgb(85, 163, 130)] to-[rgb(30, 98, 69)] text-blue-100">
-    
+    <template #content>
+      <p>TESTING</p>
+      <p>TESTING</p>
+      <p>TESTING</p>
+      <p>TESTING</p>
+      <p>TESTING</p>
+    </template>
   </Card>
 </template>
 
