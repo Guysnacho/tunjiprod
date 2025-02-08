c4c7abdbc25051a0ca3b14cee8699736b9ead93f
Guysnacho <tunjiprod@gmail.com>
2025-02-08 01:57:36 -0600

    Good starter point, now position it right and we're comfy

--- components/SOTD.vue
+++ components/SOTD.vue
@@ -3,10 +3,10 @@
         <p>{{ props.sotd.created_at?.split("T")[0] }}</p>
     </div>
     <div v-ripple class="w-[160px] h-[160px] md:w-[224px] md:h-[224px]"
-        v-styleclass="{ selector: '.box2', enterFromClass: 'hidden', enterActiveClass: 'animate-slidedown' }">
+        v-styleclass="{ selector: '.content', enterFromClass: 'hidden', enterActiveClass: 'animate-fadeinleft', enterToClass: 'visible', leaveFromClass: 'visible', leaveActiveClass: 'animate-fadeoutleft', leaveToClass: 'hidden' }">
         <NuxtImg :alt="props.sotd.name" :src="props.sotd.album_art.url" class="rounded-md" />
     </div>
-    <div class="hidden animate-duration-500 box2 overflow-hidden">
+    <div class="content hidden animate-duration-500 overflow-hidden">
         <p>{{ props.sotd.name }}</p>
         <p>{{ props.sotd.artists.join(" ") }}</p>
         <p>{{ props.sotd.album }}</p>
