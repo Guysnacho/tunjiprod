14dcdb1012a50e72476de48bb4261c655e61fbef
Guysnacho <tunjiprod@gmail.com>
2025-02-10 23:24:13 -0600

    Use NuxtLink in nav, cleans up experiance

--- components/Nav.vue
+++ components/Nav.vue
@@ -32,12 +32,12 @@ const items: MenuItem[] = [
         </template>
         <template #item="{ item, props, hasSubmenu, root }">
             <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                <NuxtLink v-ripple :href="href" v-bind="props.action" @click="navigate">
                     <span :class="item.icon" />
                     <span>{{ item.label }}</span>
-                </a>
+                </NuxtLink>
             </router-link>
-            <a v-else v-ripple class="flex items-center" v-bind="props.action">
+            <NuxtLink v-else v-ripple class="flex items-center" v-bind="props.action">
                 <span>{{ item.label }}</span>
                 <Badge v-if="item.badge" :class="{ 'ml-auto': !root, 'ml-2': root }" :value="item.badge" />
                 <span v-if="item.shortcut"
@@ -45,12 +45,12 @@ const items: MenuItem[] = [
                         item.shortcut }}</span>
                 <i v-if="hasSubmenu"
                     :class="['pi pi-angle-down ml-auto', { 'pi-angle-down': root, 'pi-angle-right': !root }]"></i>
-            </a>
+            </NuxtLink>
         </template>
         <!-- <template #end>
             <div class="flex items-center gap-2">
                 <InputText placeholder="Search" type="text" class="w-32 sm:w-auto" />
-                <Avatar image="https://ui-avatars.com/api/?name=John+Doe" shape="circle" />
+                <NuxtLinkvatar image="https://ui-avatars.com/api/?name=John+Doe" shape="circle" />
             </div>
         </template> -->
     </Menubar>
