99ab98ec1b62073a5d65c270798890e6a999bffa
Guysnacho <tunjiprod@gmail.com>
2025-02-10 22:59:58 -0600

    Title and error changes

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,23 +1,38 @@
 <template>
     <!-- Render the blog post as Prose & Vue components -->
-    <Panel class="w-5/6 mx-auto my-20">
+    <Panel v-if="data" class="w-5/6 mx-auto my-20">
         <template #header>
-            <h2 v-if="data && data.title"
+            <h2 :v-if="data && data.title"
                 class="text-3xl my-5 font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
-                {{ data.title }}
+                {{ data!.title }}
             </h2>
         </template>
         <div class="m-0">
             <ContentRenderer v-if="data" :value="data" />
         </div>
     </Panel>
+    <Panel v-else-if="error" class="w-5/6 mx-auto my-20">
+        <div class="m-0">
+            <p>We ran into an issue pulling this particular post. Try again later.</p>
+            <p>{{ error.message }}</p>
+        </div>
+    </Panel>
+    <Panel v-else class="w-5/6 mx-auto my-20 flex justify-center">
+        <ProgressSpinner class="m-auto self-center" />
+    </Panel>
 </template>
 
 <script setup lang="ts">
 const route = useRoute()
-const { data } = await useAsyncData(`blog-${route.params.slug}`, () => {
+const { data, error } = await useAsyncData(`blog-${route.params.slug}`, () => {
     return queryCollection('content').path(route.path).first()
 })
+
+if (error) {
+    console.error(error)
+} else {
+    console.debug("Successfully fetched page - ", data.value?.title)
+}
 
 useSeoMeta({
     title: data.value!.title,
