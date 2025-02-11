e12863fa253d5e16a98df5f4c314b13cd23c2567
Guysnacho <tunjiprod@gmail.com>
2025-02-10 20:08:22 -0600

    Setup blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,50 +1,96 @@
 <template>
+    <div class="w-5/6 md:w-2/3 mx-auto space-y-12">
+        <div class="h-10 flex select-none"></div>
 
-    <div class="w-5/6 md:w-2/3 mx-auto">
-        <div class="h-40 flex select-none">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl lg:max-w-4xl">
+                <h2 class="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl">Tunji's Notebook
+                </h2>
+                <p class="mt-2 text-lg/8 text-gray-700">Welcome to the Notebook. This one will contain highlights of
+                    work done,
+                    blog posts and write-ups about anything from silence to my love/hate
+                    relationship with SpringBoot, to my thoughts on Doechii winning a grammy
+                    and everything in between.</p>
+                <p class="mt-2 text-lg/8 text-gray-700">Take your time.</p>
+                <div class="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
+                    <article v-for="post in data" :key="post.id"
+                        class="relative isolate flex flex-col gap-8 lg:flex-row">
+                        <div class="relative aspect-video sm:aspect-2/1 lg:aspect-square lg:w-64 lg:shrink-0">
+                            <img v-if="post.meta.hero" :src="post.meta.hero" alt=""
+                                class="absolute inset-0 size-full rounded-2xl bg-gray-50 object-cover" />
+                            <div class="absolute inset-0 rounded-2xl ring-1 ring-gray-900/10 ring-inset" />
+                        </div>
+                        <div>
+                            <div class="flex items-center gap-x-4 text-xs">
+                                <time :datetime="post.meta.date" class="text-gray-500">{{ post.meta.date }}</time>
+                                <a :href="post.path"
+                                    class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{
+                                        post.title }}</a>
+                            </div>
+                            <div class="group relative max-w-xl">
+                                <h3 class="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600">
+                                    <a :href="post.path">
+                                        <span class="absolute inset-0" />
+                                        {{ post.title }}
+                                    </a>
+                                </h3>
+                                <p v-if="post && post.excerpt" class="mt-5 text-sm/6 text-gray-600">{{
+                                    post.excerpt.value.map(item =>
+                                        item[2]).join('.') }}</p>
+                            </div>
+                            <!-- <div class="mt-6 flex border-t border-gray-900/5 pt-6">
+                                <div class="relative flex items-center gap-x-4">
+                                    <img :src="post.author.imageUrl" alt="" class="size-10 rounded-full bg-gray-50" />
+                                    <div class="text-sm/6">
+                                        <p class="font-semibold text-gray-900">
+                                            <a :href="post.author.href">
+                                                <span class="absolute inset-0" />
+                                                {{ post.author.name }}
+                                            </a>
+                                        </p>
+                                        <p class="text-gray-600">{{ post.author.role }}</p>
+                                    </div>
+                                </div>
+                            </div> -->
+                        </div>
+                    </article>
+                </div>
+            </div>
         </div>
-        <div class="space-y-5 w-11/12 md:w-7/12 mx-auto md:mx-0">
-            <h2 class="text-3xl my-auto select-none">Tunji's Notebook</h2>
-            <p class="text-xl/7">
-                Welcome to the Notebook. This one will contain highlights of work done, blog posts and write-ups about
-                anything from silence to my love/hate relationship with SpringBoot, to my thoughts on Doechii winning a
-                grammy and everything in between.
-            </p>
-            <p class="text-xl my-auto text-gray-800">Take your time.</p>
-        </div>
-        <p :v-if="value" :v-for="article in value">article</p>
+        <!-- <p :v-if="data" :v-for="article in data">article</p> -->
     </div>
-
 </template>
 
 <script setup>
-const { data: { value } } = await useAsyncData(`blog`, () => {
-    return queryCollection('content').all()
-})
+const {
+    data,
+} = await useAsyncData(`blog`, () => {
+    return queryCollection("content").order("id", 'DESC').all();
+});
 
 useSeoMeta({
-    title: 'Notebook | Tunji Productions',
-    ogTitle: 'Notebook | Tunji Productions',
-    twitterTitle: 'Notebook | Tunji Productions',
-    ogImage: '/img/logo_bg.jpg',
-    ogUrl: 'https://tunjiproductions.com/blog',
-    twitterImage: '/img/logo_bg.jpg',
-    twitterCard: 'summary',
-    description: 'A blog of work done, discoveries, and more.',
-    ogDescription: 'A blog of work done, discoveries, and more.',
-    twitterDescription: 'A blog of work done, discoveries, and more.',
-})
+    title: "Notebook | Tunji Productions",
+    ogTitle: "Notebook | Tunji Productions",
+    twitterTitle: "Notebook | Tunji Productions",
+    ogImage: "/img/logo_bg.jpg",
+    ogUrl: "https://tunjiproductions.com/blog",
+    twitterImage: "/img/logo_bg.jpg",
+    twitterCard: "summary",
+    description: "A blog of work done, discoveries, and more.",
+    ogDescription: "A blog of work done, discoveries, and more.",
+    twitterDescription: "A blog of work done, discoveries, and more.",
+});
 
 useHead({
     htmlAttrs: {
-        lang: 'en'
+        lang: "en",
     },
     link: [
         {
-            rel: 'icon',
-            type: 'image/png',
-            href: '/favicon.ico'
-        }
-    ]
-})
+            rel: "icon",
+            type: "image/png",
+            href: "/favicon.ico",
+        },
+    ],
+});
 </script>
