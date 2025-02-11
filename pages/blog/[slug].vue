7409ad2b71c272331207069109a40ed827d4b9e4
Guysnacho <tunjiprod@gmail.com>
2025-02-10 22:11:51 -0600

    Updaaates

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -2,33 +2,34 @@
     <!-- Render the blog post as Prose & Vue components -->
     <Panel class="w-5/6 mx-auto my-20">
         <template #header>
-            <h2 class="text-3xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
+            <h2 v-if="data && data.title"
+                class="text-3xl my-5 font-semibold tracking-tight text-pretty text-gray-900 sm:text-4xl">
                 {{ data.title }}
             </h2>
         </template>
         <div class="m-0">
-            <ContentRenderer v-if="data" :value="data"  />
+            <ContentRenderer v-if="data" :value="data" />
         </div>
     </Panel>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const route = useRoute()
 const { data } = await useAsyncData(`blog-${route.params.slug}`, () => {
     return queryCollection('content').path(route.path).first()
 })
 
 useSeoMeta({
-    title: data.value.title,
-    ogTitle: data.value.title,
-    twitterTitle: data.value.title,
+    title: data.value!.title,
+    ogTitle: data.value!.title,
+    twitterTitle: data.value!.title,
     ogImage: '/img/logo_bg.jpg',
     ogUrl: 'https://tunjiproductions.com/blog',
     twitterImage: '/img/logo_bg.jpg',
     twitterCard: 'summary',
-    description: data.value.description,
-    ogDescription: data.value.description,
-    twitterDescription: data.value.description,
+    description: data.value!.description,
+    ogDescription: data.value!.description,
+    twitterDescription: data.value!.description,
 })
 
 useHead({
