11c5e624a87602a24bc9b9f906d1b22d945a8823
Guysnacho <tunjiprod@gmail.com>
2025-04-06 15:23:57 -0500

    ANALYTICS

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
 <template>
+  <!-- Google tag (gtag.js) -->
+  <Script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-GM9FF7LJKY"
+  ></Script>
+  <Script>
+    window.dataLayer = window.dataLayer || []; function
+    gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config',
+    'G-GM9FF7LJKY');
+  </Script>
+
   <NuxtLoadingIndicator />
   <div>
     <Nav />
