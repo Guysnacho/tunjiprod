41b230c2b07ff9f965c6a64e10fcdb650465e644
Guysnacho <tunjiprod@gmail.com>
2026-02-11 00:07:02 -0600

    scorched earth

--- components/content/Hero.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="space-y-5">
-        <NuxtImg :src="props.src" class="w-full h-[15rem] md:h-[30rem] overflow-hidden object-cover object-top" />
-        <p class="text-gray-600 text-center">Photo by <a :href="props.url" target="_blank">{{ props.artist }}</a></p>
-    </div>
-</template>
-<script setup lang="ts">
-const props = defineProps<{ src: string, artist: string, url: string }>()
-</script>
--- components/content/Img.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <NuxtImg :src="props.src" class="w-[30rem] h-[30rem] rounded-lg m-auto" />
-</template>
-<script setup lang="ts">
-const props = defineProps<{ src: string }>()
-</script>
